5b841a9c76b0f57e03540760b8234f5c110be343
xinyeeannie <xinyee.lim19@gmail.com>
2023-05-20 13:00:45 +0800

    commiting for keyword extraction

---
+++ back/keywords-service/keywords.py
@@ -0,0 +1,28 @@
+import yake
+
+
+def kw_extractor(text):
+    kw_extractor = yake.KeywordExtractor(top=10, stopwords=None)
+    keywords = kw_extractor.extract_keywords(text)
+    for kw, v in keywords:
+        print("Keyphrase: ",kw, ": score", v)
+
+def get_content():
+    article_path = "test_article/article.txt"
+    try:
+        with open(article_path, 'r') as file:
+            content = file.read()
+    except FileNotFoundError:
+        print(f"File '{article_path}' not found.")
+    except IOError:
+        print(f"An error occurred while reading the file '{article_path}'.")
+    return content
+
+def main():
+    text = get_content()
+    print("test")
+    kw_extractor(text)
+
+if __name__ == "__main__": 
+    
+    main()
